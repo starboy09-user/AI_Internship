6b3950cd2230b600c79d6b5dfd62884d830ba6a3
starboy09-user <vp477539@gmail.com>
2026-05-31 13:46:20 +0530

    Day 1 completed

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("=" * 40)
+print("      AI INTERNSHIP - DAY 1")
+print("=" * 40)
+
+name = "Vibhu Patel"
+
+print(f"Intern Name : {name}")
+print("Python Version : 3.13.7")
+print("Status : Environment Setup Completed")
+print("Task : First Python Program Executed Successfully")
+
+print("\nLearning Topics:")
+print("1. Artificial Intelligence (AI)")
+print("2. Machine Learning (ML)")
+print("3. Deep Learning (DL)")
+
+print("\nMy AI Internship Journey Starts Today!")
+print("=" * 40)
